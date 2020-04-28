87d256019cb52f0d260e8ef1612d0771b8c7e4f0
Abdullah <abdexking1@gmail.com>
2020-04-28 04:24:34 -0700

    first commit

---
+++ src/test/java/com/example/Myspringbootapp1/Myspringbootapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Myspringbootapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myspringbootapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
